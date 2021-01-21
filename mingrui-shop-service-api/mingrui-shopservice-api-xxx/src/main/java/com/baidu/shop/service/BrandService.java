b6baeb1ffee58bc226e50929d9af9235852ccddd
hlmb-hlmb <helianmingbo@163.com>
2021-01-21 17:56:08 +0800

    品牌新增 图片上传

---
+++ mingrui-shop-service-api/mingrui-shopservice-api-xxx/src/main/java/com/baidu/shop/service/BrandService.java
@@ -0,0 +1,40 @@
+package com.baidu.shop.service;
+
+import com.baidu.shop.base.Result;
+import com.baidu.shop.dto.BrandDTO;
+import com.baidu.shop.entity.BrandEntity;
+import com.baidu.shop.validate.group.MingruiOperation;
+import com.github.pagehelper.PageInfo;
+import com.google.gson.JsonObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+/**
+ * @ClassName BrandService
+ * @Description: TODO
+ * @Author hlmb
+ * @Date 2021/1/20
+ * @Version V1.0
+ **/
+@Api(tags = "品牌接口")
+public interface BrandService {
+    @ApiOperation(value = "查询商品列表")
+    @GetMapping(value = "brand/list")
+    Result<PageInfo<BrandEntity>> getBrandInfo(BrandDTO brandDTO);
+
+    @GetMapping(value = "brand/getBrandInfoByCategoryId")
+    @ApiOperation(value = "通过id分类查询品牌")
+    Result<List<BrandEntity>> getBrandInfoByCategoryId(Integer cid);
+
+    @ApiOperation(value = "新增品牌")
+    @PostMapping(value = "brand/save")
+    Result<JsonObject> saveBrandInfo(@Validated({MingruiOperation.Add.class}) @RequestBody BrandDTO brandDTO);
+
+}
---
+++ mingrui-shop-service/mingrui-shop-servicexxx/src/main/java/com/baidu/shop/service/impl/BrandServiceImpl.java
@@ -0,0 +1,99 @@
+package com.baidu.shop.service.impl;
+
+import com.baidu.shop.base.BaseApiService;
+import com.baidu.shop.base.Result;
+import com.baidu.shop.dto.BrandDTO;
+import com.baidu.shop.entity.BrandEntity;
+import com.baidu.shop.entity.CategoryBrandEntity;
+import com.baidu.shop.mapper.BrandMapper;
+import com.baidu.shop.mapper.CategoryBrandMapper;
+import com.baidu.shop.service.BrandService;
+import com.baidu.shop.utils.BaiduBeanUtil;
+import com.baidu.shop.utils.PinYinUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.google.gson.JsonObject;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+* @ClassName BrandServiceImpl
+* @Description: TODO
+* @Author hlmb
+* @Date 2021/1/20
+* @Version V1.0
+**/
+@RestController
+public class BrandServiceImpl extends BaseApiService implements BrandService {
+
+    @Resource
+    private BrandMapper brandMapper;
+
+    @Resource
+    private CategoryBrandMapper categoryBrandMapper;
+
+    @Override
+    public Result<JsonObject> saveBrandInfo(BrandDTO brandDTO) {
+        BrandEntity brandEntity = BaiduBeanUtil.copyProperties(brandDTO, BrandEntity.class);
+        brandEntity.setLetter(PinYinUtil.getUpperCase(String.valueOf(brandEntity.getName().toCharArray()[0]),false).toCharArray()[0]);
+
+        brandMapper.insertSelective(brandEntity);
+
+        this.insertCategoryBrandList(brandDTO.getCategories(),brandEntity.getId());
+
+        return this.setResultSuccess();
+    }
+
+
+    @Override
+    public Result<List<BrandEntity>> getBrandInfoByCategoryId(Integer cid) {
+        List<BrandEntity> list = brandMapper.getBrandInfoByCategoryId(cid);
+        return this.setResultSuccess(list);
+    }
+
+    @Override
+    public Result<PageInfo<BrandEntity>> getBrandInfo(BrandDTO brandDTO) {
+        //分页插件
+        PageHelper.startPage(brandDTO.getPage(),brandDTO.getRows());
+
+        if (!StringUtils.isEmpty(brandDTO.getSort())){
+            PageHelper.orderBy(brandDTO.getOrderBy());
+        }
+
+        BrandEntity brandEntity = BaiduBeanUtil.copyProperties(brandDTO,BrandEntity.class);
+
+        Example example = new Example(BrandEntity.class);
+        example.createCriteria().andLike("name","%"+ brandEntity.getName() +"%");
+
+        List<BrandEntity> brandEntityList = brandMapper.selectByExample(example);
+        PageInfo<BrandEntity> pageInfo = new PageInfo<>(brandEntityList);
+        return this.setResultSuccess(pageInfo);
+    }
+    //提取新增重复代码
+    private void insertCategoryBrandList(String categories, Integer brandId) {
+
+        if (StringUtils.isEmpty(categories)) throw new RuntimeException("分类信息不能为空");
+
+        if (categories.contains(",")) {
+            categoryBrandMapper.insertList(
+                    Arrays.asList(categories.split(","))
+                            .stream()
+                            .map(categoryIdStr -> new CategoryBrandEntity(Integer.valueOf(categoryIdStr)
+                                    , brandId))
+                            .collect(Collectors.toList())
+            );
+        } else {
+            CategoryBrandEntity categoryBrandEntity = new CategoryBrandEntity();
+            categoryBrandEntity.setBrandId(brandId);
+            categoryBrandEntity.setCategoryId(Integer.valueOf(categories));
+
+            categoryBrandMapper.insertSelective(categoryBrandEntity);
+        }
+    }
+}
