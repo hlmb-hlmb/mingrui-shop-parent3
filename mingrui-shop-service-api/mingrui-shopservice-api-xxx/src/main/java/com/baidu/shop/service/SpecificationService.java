e328fd005f35b0ef4ed259f8387d9afbf5371826
hlmb-hlmb <helianmingbo@163.com>
2021-01-27 18:26:44 +0800

    规格参数新增修改删除

--- mingrui-shop-service-api/mingrui-shopservice-api-xxx/src/main/java/com/baidu/shop/service/SpecificationService.java
+++ mingrui-shop-service-api/mingrui-shopservice-api-xxx/src/main/java/com/baidu/shop/service/SpecificationService.java
@@ -3,9 +3,12 @@ package com.baidu.shop.service;
 import com.baidu.shop.base.Result;
 import com.baidu.shop.dto.SpecGroupDTO;
 import com.baidu.shop.entity.SpecGroupEntity;
+import com.baidu.shop.validate.group.MingruiOperation;
+import com.google.gson.JsonObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +23,17 @@ import java.util.List;
 public interface SpecificationService {
     @ApiOperation(value = "通过条件查询规格组")
     @GetMapping(value = "/specgroup/getSpecGroupInfo")
+    
     Result<List<SpecGroupEntity>> getSpecGroupInfo(SpecGroupDTO specGroupDTO);
+    @ApiOperation(value = "新增")
+    @PostMapping(value = "/specgroup/save")
+    Result<JsonObject> saveSpecGroup(@Validated({MingruiOperation.Add.class}) @RequestBody SpecGroupDTO specGroupDTO);
+
+    @ApiOperation(value = "修改")
+    @PutMapping(value = "/specgroup/save")
+    Result<JsonObject> updateSpecGroup(@Validated({MingruiOperation.Update.class}) @RequestBody SpecGroupDTO specGroupDTO);
+
+    @ApiOperation(value = "删除")
+    @DeleteMapping(value = "/specgroup/delete/{id}")
+    Result<JsonObject> deleteSpecGroup(@PathVariable Integer id);
 }
--- mingrui-shop-service/mingrui-shop-servicexxx/src/main/java/com/baidu/shop/service/impl/SpecificationServiceImpl.java
+++ mingrui-shop-service/mingrui-shop-servicexxx/src/main/java/com/baidu/shop/service/impl/SpecificationServiceImpl.java
@@ -4,9 +4,12 @@ import com.baidu.shop.base.BaseApiService;
 import com.baidu.shop.base.Result;
 import com.baidu.shop.dto.SpecGroupDTO;
 import com.baidu.shop.entity.SpecGroupEntity;
+import com.baidu.shop.entity.SpecParamEntity;
 import com.baidu.shop.mapper.SpecGroupMapper;
+import com.baidu.shop.mapper.SpecParamMapper;
 import com.baidu.shop.service.SpecificationService;
 import com.baidu.shop.utils.BaiduBeanUtil;
+import com.google.gson.JsonObject;
 import org.springframework.web.bind.annotation.RestController;
 import tk.mybatis.mapper.entity.Example;
 
@@ -26,10 +29,41 @@ public class SpecificationServiceImpl extends BaseApiService implements Specific
     @Resource
     private SpecGroupMapper specGroupMapper;
 
+    @Resource
+    private SpecParamMapper specParamMapper;
+
     @Override
     public Result<List<SpecGroupEntity>> getSpecGroupInfo(SpecGroupDTO specGroupDTO) {
         Example example = new Example(SpecGroupEntity.class);
         example.createCriteria().andEqualTo("cid", BaiduBeanUtil.copyProperties(specGroupDTO,SpecGroupEntity.class).getCid());
         List<SpecGroupEntity> list = specGroupMapper.selectByExample(example);
-        return this.setResultSuccess(list);    }
+        return this.setResultSuccess(list);
+    }
+
+    @Override
+    public Result<JsonObject> saveSpecGroup(SpecGroupDTO specGroupDTO) {
+        specGroupMapper.insertSelective(BaiduBeanUtil.copyProperties(specGroupDTO,SpecGroupEntity.class));
+        return this.setResultSuccess();
+    }
+
+    @Override
+    public Result<JsonObject> updateSpecGroup(SpecGroupDTO specGroupDTO) {
+        specGroupMapper.updateByPrimaryKeySelective(BaiduBeanUtil.copyProperties(specGroupDTO,SpecGroupEntity.class));
+        return this.setResultSuccess();
+    }
+
+    @Override
+    public Result<JsonObject> deleteSpecGroup(Integer id) {
+        //判断规格组之前是否有规格参数
+        Example example = new Example(SpecParamEntity.class);
+        example.createCriteria().andEqualTo("groupId",id);
+
+        List<SpecParamEntity> specParamEntities = specParamMapper.selectByExample(example);
+
+        if(specParamEntities.size() > 0){
+            return this.setResultError("不能删除此参数");
+        }
+        specGroupMapper.deleteByPrimaryKey(id);
+        return this.setResultSuccess();
+    }
 }
